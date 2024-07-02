53c74329d094ec75be48adaba6517c912cebf031
Alexandre <alexandre.bueno0204@gmail.com>
2024-07-02 16:22:17 -0300

    v1 dashboard

---
+++ v1dashboard.py
@@ -0,0 +1,25 @@
+import requests as req
+
+# funcao para ler a chave de chave.txt
+
+def ler_chave():
+    with open('chave.txt', 'r') as file:
+        for line in file:
+            if line.startswith('chave_ab'):
+                return line.split('=')[1].strip()
+    return None
+
+URL_BASE = 'https://api.fintz.com.br'
+HEADERS = { 'X-API-Key': ler_chave() } # minha chave
+PARAMS = { 'ticker': 'BBAS3', 'dataInicio': '2024-06-03'} # dataFim opcional
+
+endpoint = URL_BASE + '/bolsa/b3/avista/cotacoes/historico'
+res = req.get(endpoint, headers=HEADERS, params=PARAMS)
+print(res.json())
+
+
+
+# apenas dados precos de fechamento ->
+# data = res.json()
+# precos_fechamento = [item['precoFechamento'] for item in data]
+# print(precos_fechamento)
